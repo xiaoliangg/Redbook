aa042eeed140136297caeb7504e4a23d9a4b7610
xiaoliangg <xiaoliangyuu@163.com>
2022-04-05 11:13:18 +0800

    first commit

---
+++ Redbook/RedbookApp.swift
@@ -0,0 +1,17 @@
+//
+//  RedbookApp.swift
+//  Redbook
+//
+//  Created by yl on 2022/4/5.
+//
+
+import SwiftUI
+
+@main
+struct RedbookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
